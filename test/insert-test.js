da462f954b1531616649f3da471b885752473c5e
Sean Walker <swlkr.rbl@gmail.com>
2014-10-01 21:30:11 -0700

    Add returning statement support

--- test/insert-test.js
+++ test/insert-test.js
@@ -7,4 +7,14 @@ describe('insert', function() {
     expect(sql.insert('users', { firstName: 'Johnny', lastName: 'Appleseed' }).toQuery())
       .to.deep.equal({text: 'insert into users (firstName, lastName) values ($1, $2)', values: ['Johnny', 'Appleseed']});
   });
+
+  it('should generate a returning statement', function() {
+    expect(sql.insert('users', { firstName: 'Johnny', lastName: 'Appleseed' }).returning().toQuery())
+      .to.deep.equal({text: 'insert into users (firstName, lastName) values ($1, $2) returning *', values: ['Johnny', 'Appleseed']});
+  });
+
+  it('should generate a returning statement with columns', function() {
+    expect(sql.insert('users', { firstName: 'Johnny', lastName: 'Appleseed' }).returning('firstName', 'lastName').toQuery())
+      .to.deep.equal({text: 'insert into users (firstName, lastName) values ($1, $2) returning firstName, lastName', values: ['Johnny', 'Appleseed']});
+  });
 });
