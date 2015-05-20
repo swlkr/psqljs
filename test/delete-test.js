615914d01cc54823d51ba42d711195c4da0b37d4
Sean Walker <swlkr.rbl@gmail.com>
2015-05-19 19:13:56 -0700

    Clean up iffy logic. ' -> "

--- test/delete-test.js
+++ test/delete-test.js
@@ -1,10 +1,10 @@
-var chai = require('chai'),
+var chai = require("chai"),
     expect = chai.expect,
-    sql = require('../psql');
+    sql = require("../psql");
 
-describe('delete', function() {
-  it('should generate the correct sql statement', function() {
-    expect(sql.delete('users').where('firstName = ?', 'Johnny').toQuery())
-      .to.deep.equal({text: 'delete from users where firstName = $1', values: ['Johnny']});
+describe("delete", function() {
+  it("should generate the correct sql statement", function() {
+    expect(sql.delete("users").where("firstName = $1", "Johnny").toQuery())
+      .to.deep.equal({text: "delete from users where firstName = $1", values: ["Johnny"]});
   });
 });
